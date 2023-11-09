fe14e90f180c5af3aff14cb0ea969a553bf69737
FH <surfaceleek6@gmail.com>
2023-11-09 18:00:40 -0500

    demo 2 complete

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using CampusOrdering.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusOrdering.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerRepository _customerRepository;
+
+        public CustomerController()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
+        public IActionResult Settings(int customerId = 0)
+        {
+            var customer = _customerRepository.GetCustomerById(customerId);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+
+        public IActionResult Edit(int customerId)
+        {
+            var customer = _customerRepository.GetCustomerById(customerId);
+            if (customer == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer editedCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                _customerRepository.UpdateCustomer(editedCustomer);
+                return RedirectToAction("Settings", new { customerId = editedCustomer.Id });
+            }
+
+            return View(editedCustomer);
+        }
+
+    }
+
+
+}
+
---
+++ Models/CustomerRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace CampusOrdering.Models
+{
+    public class CustomerRepository
+    {
+        private List<Customer> customers = new List<Customer>();
+
+        public CustomerRepository()
+        {
+            customers.Add(new Customer { Id = 0, Name = "Jane", Birthdate = DateTime.Parse("1990-01-15"), Email = "Jane@example.com", Password = "password0" });
+
+            customers.Add(new Customer { Id = 1, Name = "Alice", Birthdate = DateTime.Parse("1990-01-15"), Email = "alice@example.com", Password = "password1" });
+            customers.Add(new Customer { Id = 2, Name = "Bob", Birthdate = DateTime.Parse("1985-07-22"), Email = "bob@example.com", Password = "password2" });
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void UpdateCustomer(Customer updatedCustomer)
+        {
+            var existingCustomer = customers.FirstOrDefault(c => c.Id == updatedCustomer.Id);
+            if (existingCustomer != null)
+            {
+                existingCustomer.Name = updatedCustomer.Name;
+                existingCustomer.Birthdate = updatedCustomer.Birthdate;
+                existingCustomer.Email = updatedCustomer.Email;
+                existingCustomer.Password = updatedCustomer.Password;
+            }
+        }
+    }
+
+}
