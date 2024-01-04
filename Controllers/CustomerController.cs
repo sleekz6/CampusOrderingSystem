660c493ace76a7e83f43afd0e46ffbcfd1af07eb
Rafe Mohammad <n01450467@unf.edu>
2024-01-04 12:43:00 -0500

    AddedCustomerTableData

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,20 +1,33 @@
 ﻿using CampusOrdering.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace CampusOrdering.Controllers
 {
     public class CustomerController : Controller
     {
-        private readonly CustomerRepository _customerRepository;
+       
+        private readonly OrderingContext _context;
 
-        public CustomerController()
+
+        public CustomerController(OrderingContext context)
         {
-            _customerRepository = new CustomerRepository();
+            _context = context;
         }
 
-        public IActionResult Settings(int customerId = 0)
+        protected override void Dispose(bool disposing)
         {
-            var customer = _customerRepository.GetCustomerById(customerId);
+            _context.Dispose();
+        }
+
+
+        public IActionResult Settings(int customerId)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == customerId);
+
             if (customer == null)
             {
                 return NotFound();
@@ -26,7 +39,7 @@ namespace CampusOrdering.Controllers
 
         public IActionResult Edit(int customerId)
         {
-            var customer = _customerRepository.GetCustomerById(customerId);
+            var customer = _context.Customers.SingleOrDefault(m => m.Id == customerId);
             if (customer == null)
             {
                 return NotFound(); 
@@ -35,17 +48,36 @@ namespace CampusOrdering.Controllers
             return View(customer);
         }
 
+
+
         [HttpPost]
-        public IActionResult Edit(Customer editedCustomer)
+        public IActionResult Save(Customer editedCustomer)
         {
             if (ModelState.IsValid)
             {
-                _customerRepository.UpdateCustomer(editedCustomer);
-                return RedirectToAction("Settings", new { customerId = editedCustomer.Id });
+                var existingCustomer = _context.Customers.Find(editedCustomer.Id);
+
+                if (existingCustomer.Id != 0)
+                {
+                    
+                    existingCustomer.Name = editedCustomer.Name;
+                    existingCustomer.Birthdate = editedCustomer.Birthdate;
+                    existingCustomer.Email = editedCustomer.Email;
+                    existingCustomer.Password = editedCustomer.Password;
+
+                   
+                }
+                else
+                {
+                   
+                        _context.Customers.Add(editedCustomer);
+                }
+                _context.SaveChanges();
             }
 
-            return View(editedCustomer);
+            return RedirectToAction("Settings", new { customerId = editedCustomer.Id });
         }
+
 
     }
 
--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -7,4 +7,5 @@
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
+
 }
